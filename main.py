49fdbb1ec392453e35dc1230b7f77c8a3508a34a
Ashton Jordan <40682488+ashjorda@users.noreply.github.com>
2024-07-15 15:16:48 -0500

    Update main.py
    Deleted spacing to see if the autopep8 check will fail

--- main.py
+++ main.py
@@ -22,7 +22,6 @@ from art import logo
 import random
 import os
 
-
 def blackjack():
     print(logo)
     # Hint 1: Go to this website and try out the Blackjack game:
@@ -108,7 +107,6 @@ def blackjack():
     # Hint 12: Once the user is done, it's time to let the computer play. The
     # computer should keep drawing cards as long as it has a score less than
     # 17.
-
     while computer_score != 0 and computer_score < 17:
         computer_cards.append(deal_card())
         computer_score = calculate_score(computer_cards)
@@ -144,8 +142,6 @@ def blackjack():
     # Hint 14: Ask the user if they want to restart the game. If they answer
     # yes, clear the console and start a new game of blackjack and show the
     # logo from art.py
-
-
 while input("Do you want to play a game of Blackjack? Type 'y' or 'n': ") == 'y':
     os.system('clear')
     blackjack()
